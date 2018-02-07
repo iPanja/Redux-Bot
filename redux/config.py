270ed92f6f3521caf366274ed59b7097898e5ff4
iPanja <fhenneman@comcast.net>
2018-02-07 17:34:27 -0500

    1.3.0

---
+++ redux/config.py
@@ -0,0 +1,20 @@
+discord = dict(
+    key = DISCORD_BOT_KEY
+)
+google = dict(
+    urlshorten_key = GOOGLE_URLSHORTEN_API_KEY
+)
+oxford = dict(
+    app_id = OXFORD_APP_ID,
+    app_key = OXFORD_APP_KEY,
+    language = "en",
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'
+)
+fortnite = dict(
+    url = "https://fortnitetracker.com/profile/",
+    weapon_data_loc = "mods\json\weapon_data.json" #Look in the /tools/ directory to scrape this information into a json file for yourself
+)
+moderation = dict(
+    word_blacklist = ['hell', '12345],
+    memelist = ['Do you know de wae', 'woweee', "It's only game, why you have to be mad"]
+)
